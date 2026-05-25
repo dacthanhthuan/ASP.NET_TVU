c6c82282892ef79bbc0b9bc53144dd607e0aad73
Dac Thuan <dacthua@gmail.com>
2026-05-25 14:39:40 +0700

    first commit

---
+++ LMS/LMS/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "123")
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Error = "Sai tài khoản hoặc mật khẩu";
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(string username, string password)
+    {
+        // demo: sau này lưu DB
+        return RedirectToAction("Login");
+    }
+
+
+}
---
+++ LMS/LMS/Models/USER.cs
@@ -0,0 +1,7 @@
+﻿namespace LMS.Models
+{
+    public class USER
+    {
+
+    }
+}
